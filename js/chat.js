8e9def949985e84fae95c3f43dfb532adba2fe24
FIRST_NAME LAST_NAME <nikagermanishvili5@gmail.com>
2022-11-01 00:04:35 +0400

    final-test

---
+++ js/chat.js
@@ -0,0 +1,11 @@
+
+
+function showchatbox(){
+    document.getElementById("chatbox").style.display = "block";
+  }
+  
+  function hidechatbox(){
+    document.getElementById("chatbox").style.display = "none";
+  }
+
+  
